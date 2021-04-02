b7e3953b1cae2f67faa589097eec9b224933986c
litooo21 <78019875+litooo21@users.noreply.github.com>
2021-04-02 13:24:26 +0300

    Update 03.rb
    add indent start of line  27, 30, 33, 36, 39 27, 30, 33, 36, 39

--- 03.rb
+++ 03.rb
@@ -24,17 +24,17 @@ c = weight_triangle
 
 if
 (a == b && a == c && b == c)
-puts "triangle равносторонний"
+  puts "triangle равносторонний"
 elsif
 (b == a || b == c || a == c)
-puts "triangle равнобедренный"
+ puts "triangle равнобедренный"
 elsif
 (a > b && a > c)
-puts "#{b**2 + c**2 <= a**2}" "прямоугольный"
+ puts "#{b**2 + c**2 <= a**2}" "прямоугольный"
 elsif
 (b > a && b > c)
-puts "#{a**2 + c**2 <= b**2}" "прямоугольный"
+ puts "#{a**2 + c**2 <= b**2}" "прямоугольный"
 elsif
 (c > b && c > a)
-puts "#{a**2 + b**2 <= c**2}" "прямоугольный"
+ puts "#{a**2 + b**2 <= c**2}" "прямоугольный"
 end
